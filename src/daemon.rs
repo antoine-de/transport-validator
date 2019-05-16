a34d1088ee704bba1f2d33dac52dc47d45e32447
antoine-de <antoine.desbordes@gmail.com>
2019-05-16 09:47:55 +0200

    review

--- src/daemon.rs
+++ src/daemon.rs
@@ -23,14 +23,14 @@ Use it with /validate?url=https.//.../gtfs.zip"#
 }
 
 #[post("/validate")]
-fn validate_post((params, body): (web::Query<Params>, web::Payload)) -> impl Future<Item = Json<Response>, Error = Error> {
+fn validate_post(params: web::Query<Params>, body: web::Payload) -> impl Future<Item = Json<Response>, Error = Error> {
     let max_size = params.max_size.unwrap_or(1000);
     body.map_err(Error::from)
         .fold(web::BytesMut::new(), move |mut body, chunk| {
             body.extend_from_slice(&chunk);
             Ok::<_, Error>(body)
         })
-        .and_then(|body| Ok(Json(create_issues_post(body, max_size.clone()))))
+        .and_then(move |body| Ok(Json(create_issues_post(body, max_size))))
 }
 
 pub fn run_server() {
